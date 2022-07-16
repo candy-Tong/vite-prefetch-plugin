9913e45150a607e86388f39ece0343c7d60a3d06
candychuang <candychuang@tencent.com>
2022-07-16 10:14:00 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,76 @@
 module.exports = {
   root: true,
-  extends: [
-    '@tencent/eslint-config-tencent',
-    '@tencent/eslint-config-tencent/ts',
-    '@tencent/eslint-config-tencent/prettier',
-    'plugin:vue/vue3-recommended',
-  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
     sourceType: 'module',
+    jsxPragma: 'React',
     ecmaFeatures: {
       jsx: true,
-      tsx: true,
     },
-    extraFileExtensions: ['.vue'],
-    project: ['./tsconfig.eslint.json'],
-    tsconfigRootDir: __dirname,
   },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   rules: {
-    '@typescript-eslint/naming-convention': ['off'],
-    '@typescript-eslint/consistent-type-assertions': [
+    'vue/script-setup-uses-vars': 'error',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
       'error',
       {
-        assertionStyle: 'as',
-        objectLiteralTypeAssertions: 'allow',
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
       },
     ],
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    'space-before-function-paren': 'off',
 
-    // 与prettier冲突
+    'vue/attributes-order': 'off',
+    'vue/one-component-per-file': 'off',
+    'vue/html-closing-bracket-newline': 'off',
     'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
     'vue/singleline-html-element-content-newline': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-explicit-emits': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    'vue/multi-word-component-names': 'off',
   },
 };
